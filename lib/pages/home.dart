50d7ab42420090879d83c2cbf7132923f720d880
King Elisha <ek.chibueze@gmail.com>
2019-04-13 22:07:50 +0100

    home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('GOALZZ'),
+        centerTitle: true,
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
