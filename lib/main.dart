aba26fb7c50fc5e172776c00ec761c89c2120ee8
King Elisha <ek.chibueze@gmail.com>
2019-04-14 08:57:13 +0100

    updated fonts

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'Rubik'
       ),
       home: HomePage(),
     );
