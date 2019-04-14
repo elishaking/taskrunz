99f1313e8d04b17beb060d8cc69c9e66043ceb67
King Elisha <ek.chibueze@gmail.com>
2019-04-14 17:08:08 +0100

    responsive -1

--- lib/pages/task.dart
+++ lib/pages/task.dart
@@ -16,11 +16,20 @@ class TaskPage extends StatefulWidget{
 }
 
 class _TaskPageState extends State<TaskPage>{
+  double _targetWidth = 0;
+
+  double _getSize(final double default_1440){
+    return (default_1440 / 14) * (0.0027 * _targetWidth + 10.136);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final double proressIndicatorWidth = 230;
+    _targetWidth = MediaQuery.of(context).size.width;
+
+    final double proressIndicatorWidth = _getSize(410);
     final double progress = ((double.parse(widget.taskGroup.numTasksCompleted) / double.parse(widget.taskGroup.numTask)) * proressIndicatorWidth).roundToDouble();
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -41,9 +50,11 @@ class _TaskPageState extends State<TaskPage>{
               tag: 'icon' + widget.taskGroup.idx.toString(),
               child: Icon(widget.taskGroup.icon, color: widget.taskGroup.color,),
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 20,),
+            customText.BodyText(text: '${widget.taskGroup.numTask} Tasks', textColor: Colors.grey,),
+            SizedBox(height: 3,),
             customText.HeadlineText(text: widget.taskGroup.name, textColor: Colors.black,),
-            SizedBox(height: 15,),
+            SizedBox(height: 20,),
               // Hero(
               //   tag: 'progress' + widget.taskGroup.idx.toString(),
               //   child: 
@@ -60,8 +71,21 @@ class _TaskPageState extends State<TaskPage>{
                   ],
                 ),
               // )
+            SizedBox(height: 20,),
+            customText.BodyText(text: 'TODAY', textColor: Colors.grey,),
+            SizedBox(height: 20,),
+            Column(
+              children: List.generate(length, generator),
+            )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          
+        },
+        backgroundColor: widget.taskGroup.color,
       ),
     );
   }
