de522618083a9e7fe81cffa42651d6e1c1683d4e
King Elisha <ek.chibueze@gmail.com>
2019-04-14 17:20:48 +0100

    db boiler plate

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -10,3 +10,24 @@ class TaskGroup{
 
   TaskGroup({this.idx, this.icon, this.name, this.numTask, this.numTasksCompleted, this.color});
 }
+
+class Task{
+  final int id;
+  final String info;
+  final DateTime dateTime;
+  final bool done;
+
+  Task({this.id, this.info, this.dateTime, this.done});
+
+  Map<String, dynamic> toMap() {
+    var taskMap = <String, dynamic>{
+      'info': info,
+      'dateTime': dateTime,
+      'done': done
+    };
+    if (id != null) {
+      taskMap['_id'] = id;
+    }
+    return taskMap;
+  }
+}
