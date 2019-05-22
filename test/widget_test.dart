8e568c3450c7438498fc5a4e7c2e887a3e9f204a
King Elisha <ek.chibueze@gmail.com>
2019-05-22 23:53:30 +0100

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:goalzz/main.dart';
+import 'package:taskrunz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
